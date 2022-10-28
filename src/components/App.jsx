8534cc308d1e03bbcc9339ee476cf797088d1943
Лілія <lily.fed85@gmail.com>
2022-10-28 17:23:29 +0300

    practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import { Button } from './Button/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUsers } from 'redux/users/usersOperations';
+import { UsersList } from './UsersList/UsersList';
+import { EmailFilterUsers } from './EmailFilterUsers/EmailFilterUsers';
+import { selectUsers } from 'redux/users/usersSelectors';
+
 export const App = () => {
+  const users = useSelector(selectUsers);
+  const dispatch = useDispatch();
+  const handleClick = () => {
+    dispatch(fetchUsers());
+  };
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      {users.length > 0 ? (
+        <>
+          <EmailFilterUsers />
+          <UsersList />
+        </>
+      ) : (
+        <Button text="Show users" type="button" clickHandler={handleClick} />
+      )}
+    </>
   );
 };
